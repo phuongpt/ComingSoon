2f842c5f921ee72dba91b728d50cddf384f8176c
Phuong Phan <phuongphantai@gmail.com>
2023-07-18 09:26:54 +0700

    Add email support

--- pages/api/notifyMe.ts
+++ pages/api/notifyMe.ts
@@ -1,33 +1,20 @@
-
-
-import mongoose from 'mongoose';
-
-// Connect to the MongoDB database
-mongoose.connect('mongodb+srv://vercel:13572468@duolanguage.ogwfosp.mongodb.net/?retryWrites=true&w=majority', {
-
-});
-
-// Create a schema for the email collection
-const emailSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-  },
-});
-
-// Create a model based on the schema
-const Email = mongoose.model('Email', emailSchema);
-
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { email } = req.body; // Assuming the email is sent in the request body
 
     try {
-      // Create a new email document
-      const newEmail = new Email({ email });
 
-      // Save the email to the database
-      await newEmail.save();
+      await fetch(`https://parseapi.back4app.com/classes/Email`, {
+        method: 'POST',
+        headers: {
+          'X-Parse-Application-Id': process.env.NEXT_PUBLIC_DEFAULT_PARSE_ID,
+          'X-Parse-REST-API-Key': process.env.NEXT_PUBLIC_DEFAULT_PARSE_REST,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+        }),
+      })
 
       res.status(200).end('Email stored successfully');
     } catch (error) {
